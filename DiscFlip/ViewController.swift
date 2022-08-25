450c907138e8b26cf1bf1714bc183056a42af74a
Brian Aguirre <aguirre.28@osu.edu>
2022-08-25 09:36:44 -0400

    Initial Commit

---
+++ DiscFlip/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiscFlip
+//
+//  Created by Aguirre, Brian P. on 8/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
