41fad082bca7a85f20e04e046610ed1d08d7a6e8
Brian Aguirre <aguirre.28@osu.edu>
2022-08-30 12:24:52 -0400

    create add/edit table view controller file; initial implementation of inventory table view controller

--- DiscFlip/Controllers/AddEditDiscTableViewController.swift
+++ DiscFlip/Controllers/AddEditDiscTableViewController.swift
@@ -1,48 +1,45 @@
 //
-//  InventoryTableViewController.swift
+//  AddEditDiscTableViewController.swift
 //  DiscFlip
 //
-//  Created by Aguirre, Brian P. on 8/29/22.
+//  Created by Aguirre, Brian P. on 8/30/22.
 //
 
 import UIKit
 
-class InventoryTableViewController: UITableViewController {
-    
-    var inventory: [Item] = [Disc(name: "Thunderbird", purchasePrice: 17, plastic: "Champion")]
+class AddEditDiscTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        // #warning Incomplete implementation, return the number of sections
+        return 0
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return inventory.count
+        // #warning Incomplete implementation, return the number of rows
+        return 0
     }
 
+    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "InventoryCell", for: indexPath)
-        let item = inventory[indexPath.row]
-        
-        var content = cell.defaultContentConfiguration()
-        
-        var primaryText = item.name
-        if let disc = item as? Disc {
-            primaryText = "\(disc.plastic) " + primaryText
-        }
-        content.text = primaryText
-        
-        content.secondaryText = "Purchase price: $\(item.purchasePrice)"
-        
-        cell.contentConfiguration = content
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
 
         return cell
     }
+    */
 
     /*
     // Override to support conditional editing of the table view.
